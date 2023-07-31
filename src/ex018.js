6f716e3c16791bffc9e63639eb6510661dd6eb7b
nbogie <neillbogie@googlemail.com>
2023-07-31 16:03:52 +0100

    ex018.js name changes only

--- src/ex018.js
+++ src/ex018.js
@@ -1,7 +1,7 @@
-const startPos = { x: 0, y: 0 }
+const startPos = { x: 0, y: 0 };
 
-const a = { pos: startPos, name: "ali" }
+const a = { pos: startPos, name: "Ali" };
 
-const b = { pos: startPos, name: "george" }
+const b = { pos: startPos, name: "Foreman" };
 
 a.pos.x++;
