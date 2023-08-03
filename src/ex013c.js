36a97380804f33e978bc5ab8664dc1b83a59354e
nbogie <neillbogie@googlemail.com>
2023-08-03 09:09:50 +0100

    functions first

--- src/ex013c.js
+++ src/ex013c.js
@@ -1,11 +1,14 @@
-const a = [{ n: "stevie" }, { n: "marvin" }, { n: "aretha" }];
-
-const b = a.map(f);
-//Question: before seeing f, could a be modified by b = a.map(f)?
-
 function f(obj) {
     obj.len = obj.n.length;
     return obj.len;
 }
+
+const a = [{ n: "stevie" }, { n: "marvin" }, { n: "aretha" }];
+//take a screenshot of your diagram at this point
+
+//Question: without considering function f,
+//could array `a` be mutated by the next line?
+
+const b = a.map(f);
 
 a[1] = "hi";
