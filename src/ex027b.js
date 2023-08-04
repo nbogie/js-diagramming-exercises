15ee0a3e305ee0bf2aaead03c031990aa199de45
nbogie <neillbogie@googlemail.com>
2023-08-04 08:58:56 +0100

    add 027b and 034

---
+++ src/ex027b.js
@@ -0,0 +1,10 @@
+function myFn(arr) {
+    let x = arr[0];
+    let y = arr[1];
+    let z = arr[2];
+    y = 99;
+}
+
+const pos = [10, 20, 30];
+myFn(pos);
+//Q: what will be in pos now?
---
+++ src/ex034.js
@@ -0,0 +1,6 @@
+function myFn(a) {
+    a++;
+}
+const x = 10;
+myFn(x);
+//what's in x now?
