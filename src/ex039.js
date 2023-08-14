98ddbe37633b3407a3ceca3e92d2329275b83876
nbogie <neillbogie@googlemail.com>
2023-08-14 22:54:37 +0100

    add ex039 and ex040 (spread of arrays)

---
+++ src/ex039.js
@@ -0,0 +1,5 @@
+let original = [10, 20, 30];
+let copied = [...original];
+copied[0] = 42;
+
+console.log(original);
---
+++ src/ex040.js
@@ -0,0 +1,5 @@
+let original = [{ v: 10 }, { v: 20 }, { v: 30 }];
+let copied = [...original];
+copied[0].v = "c";
+
+console.log(original);
