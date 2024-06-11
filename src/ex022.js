ecaebd292a957c0f29d307bf9bf6354e274bf975
nbogie <neillbogie@googlemail.com>
2024-06-11 09:21:49 +0100

    minor

--- src/ex022.js
+++ src/ex022.js
@@ -1,5 +1,9 @@
-let a = ["very", "important"]
+let a = ["very", "important"];
 
-let b = [10, 20]
+let b = [10, 20];
 
-a = b
+a = b;
+
+//Q1: what's in a?  what's in b?
+//Q2: At the end, what could we write to print
+//    the first element of the strings array?
--- src/ex023.js
+++ src/ex023.js
@@ -1,9 +1,6 @@
 function myFn() {
-
     let a = "important data";
-
     return 10;
-
 }
 
 const b = myFn();
